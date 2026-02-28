41dbc064af991d98928d21b6a8319e3ada42515e
Mahmoud Hassan <mahmoudhassan7764@outlook.com>
2026-02-28 03:28:45 +0200

    add date service concrete class

---
+++ Assignment1/DataServiceAstraction/DataServiceAbstraction/DataService.cs
@@ -0,0 +1,19 @@
+namespace DataServiceAbstraction;
+
+public class DataService : IDataService
+{
+    private readonly string _filePath;
+
+    public DataService(string filePath)
+    {
+        if(filePath is null)
+            throw new ArgumentNullException(nameof(filePath));
+        if (!File.Exists(_filePath))
+            throw new FileNotFoundException(_filePath);
+        _filePath = filePath;
+    }
+    public IEnumerable<string> GetLines()
+    {
+        return File.ReadAllLines(_filePath);
+    }
+}
